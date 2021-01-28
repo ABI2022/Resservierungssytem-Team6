0d54a9707ca798a691abf405d45437cb77e5d73b
Kurt Brunner <Kontakt@Karlosbubi.de>
2021-01-28 16:12:23 +0100

    Klasse Block: Methoden Deklariert

--- Block.h
+++ Block.h
@@ -5,10 +5,20 @@
 class Block
 {
 private:
-	const int REIHEN = 50;
-	const int PLAETZE = 200;
+	const static int REIHEN = 50;
+	const static int PLAETZE = 200;
 
 	string bezeichnung;
 	bool belegung[REIHEN][PLAETZE];
+
+public:
+	bool istFrei(int r, int p);
+	void belegePlatz(int r, int p);
+	void gibPlatzFrei(int r, int p);
+
+	Block(string name);
+	Block();
+	~Block();
+
 };
 
