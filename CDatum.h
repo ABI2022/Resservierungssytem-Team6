7789740f21f2c0799407bcea73b5af1d730c5fda
Sascha <diez.s@gmx.de>
2021-02-03 09:06:55 +0100

    CDatum hinzugefügt

---
+++ CDatum.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class CDatum
+{
+private:
+	int tag, monat, jahr;
+
+
+
+public:
+	// Konstruktoren und Destruktor
+	CDatum();// Stadndartkonstruktor
+	CDatum(CDatum&);// Kopierkonstruktor
+	CDatum(int, int, int);// Parameterkonstruktor
+	CDatum(string);// Parameterkonstruktor
+	~CDatum();
+
+	//Getter
+	int getTag();
+	int getMonat();
+	int getJahr();
+	void getDatum(int&, int&, int&);
+	string getStringDatum();
+
+	//Setter
+	void setTag(int);
+	void setMonat(int);
+	void setJahr(int);
+	void setStringDatum(string);
+	void setDatum(int, int, int);
+};
+
