94c71f57b9b190a6ad68c1231c90fee12a3171b0
Sascha <diez.s@gmx.de>
2021-02-03 10:11:44 +0100

    includes in Main eingefügt

--- Resservierungssytem-Team6.cpp
+++ Resservierungssytem-Team6.cpp
@@ -1,7 +1,9 @@
 #include "util.h"
-
+#include "Reservierung.h"
 #include "Block.h"
-
+#include "CDatum.h"
+#include "Kunde.h"
+//#include "Spiel.h"
 int main()
 {
     std::cout << "Hello World!\n";
